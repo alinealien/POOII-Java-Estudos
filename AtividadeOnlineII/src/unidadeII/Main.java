b18ad3fff300e62539ad82b2145dc28a1e906c02
alinealien <alinebdantunes@gmail.com>
2022-05-15 16:16:59 -0300

    Instanciando objetos na Main

--- AtividadeOnlineII/src/unidadeII/Main.java
+++ AtividadeOnlineII/src/unidadeII/Main.java
@@ -3,14 +3,14 @@ package unidadeII;
 /*
 Na classe Main:
 
-1.       Instanciar pelo menos 2 objetos da classe Aluno.
+1. Instanciar pelo menos 2 objetos da classe Aluno.
 
-2.       Instanciar pelo menos 1 objeto da classe Turma.
+2. Instanciar pelo menos 1 objeto da classe Turma.
 
-3.       Associar pelo menos 2 objetos da classe Aluno a um único objeto da classe Turma.
+3. Associar pelo menos 2 objetos da classe Aluno a um único objeto da classe Turma.
 
-4.       Imprimir uma lista com o nome de todos os Alunos
-         que fazem parte de uma Turma. Dica: Implemente um método GetAlunos.
+4. Imprimir uma lista com o nome de todos os Alunos
+   que fazem parte de uma Turma. Dica: Implemente um método GetAlunos.
  */
 
 public class Main {
@@ -18,7 +18,21 @@ public class Main {
         System.out.println("\n Atividade Online II");
         System.out.println("\n");
 
-       
+        // Instanciar pelo menos 2 objetos da classe Aluno.
+        Alunos A1 = new Alunos("090909", 2018, "Computação", "Aline Antunes", "Areia Branca", "0909087654");
+
+        //Instanciar pelo menos 1 objeto da classe Turma.
+        Turma T1 = new Turma("Sala A1", "Computação");
+        Turma T2 = new Turma("Sala A2", "Física");
+
+        //Associar pelo menos 2 objetos da classe Aluno a um único objeto da classe Turma. - COMO ASSIM?
+        
+
+
+        System.out.println("Lista dos Alunos: ");
+        System.out.println();
+        //System.out.println(A1.getNome());
+
 
 
     }
